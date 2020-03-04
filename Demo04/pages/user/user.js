b23e5444dcab0589b41174aa513d1639e8ccc108
chenlinqiang <327371999@qq.com>
2020-03-04 23:45:03 +0800

    龙虎传奇

--- Demo04/pages/user/user.js
+++ Demo04/pages/user/user.js
@@ -2,52 +2,71 @@
 const app = getApp()
 
 Page({
+
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    phone: "",
+    password: "",
+    hasLogin:false,
   },
-  //事件处理函数
-  bindViewTap: function () {
-    wx.navigateTo({
-      url: '../play/play'
+  onLoad: function() {
+
+  },
+  bindPhoneInput: function(e) {
+    this.setData({
+      phone: e.detail.value
     })
   },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse) {
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
+  bindPasswordInput: function(e) {
+    this.setData({
+      password: e.detail.value
+    })
+  },
+  login: function(e) {
+    var that=this
+    wx: wx.showToast({
+      title: '登录请求中',
+      icon: 'loading',
+      image: '',
+      duration: 1000,
+      mask: true,
+      success: function(res) {
+
+      },
+      fail: function(res) {},
+      complete: function(res) {},
+    });
+    //requese
+    wx: wx.request({
+
+      url: 'https://www.spe.kim/Legend/Login',
+      header: {
+        'Content-Type': 'application/json'
+      },
+      method: 'POST',
+      dataType: 'json',
+      responseType: 'text',
+      success: function(res) {
+        console.log(res);
+        wx.hideToast();
+        if (res.data.LoginStatus == "OK") {
+          //jump
+          that.setData({
+            hasLogin:true,
+          })
+
+        } else {
+          wx.showModal({
+            title: '登录失败',
+            content: '请检查您填写的用户信息',
+            showCancel: false,
+            success: function(res) {},
           })
         }
-      })
-    }
-  },
-  getUserInfo: function (e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+      },
+      fail: function(res) {},
+      complete: function(res) {},
     })
+
   }
+
 })
